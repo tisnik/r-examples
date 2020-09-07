e182c7af7bfee5ae54e8f33ac6b07bed9b75d284
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-09-07 09:20:11 +0200

    Example 03: column and row vector as an array

---
+++ 03-arrays/03-array-construction-1D-column-vector.R
@@ -0,0 +1,15 @@
+data <- c(1, 2, 3, 4, 5, 6)
+dimension1 <- c(6)
+
+a1 <- array(data, dim=dimension1)
+print(a1)
+
+dimension2 <- c(1, 6)
+
+a2 <- array(data, dim=dimension2)
+print(a2)
+
+dimension3 <- c(6, 1)
+
+a3 <- array(data, dim=dimension3)
+print(a3)
