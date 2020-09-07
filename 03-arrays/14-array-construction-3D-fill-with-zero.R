d60511db91ed4332c0a3ce7e7bc7352e904ea1d1
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-09-07 09:20:11 +0200

    Example 14: creating 3D array filled in zeroes

---
+++ 03-arrays/14-array-construction-3D-fill-with-zero.R
@@ -0,0 +1,16 @@
+data <- 0
+
+print("2x3x4")
+dimension1 <- c(2, 3, 4)
+a1 <- array(data, dim=dimension1)
+print(a1)
+
+print("3x2x4")
+dimension2 <- c(3, 2, 4)
+a2 <- array(data, dim=dimension2)
+print(a2)
+
+print("4x3x2")
+dimension3 <- c(4, 3, 2)
+a3 <- array(data, dim=dimension3)
+print(a3)
