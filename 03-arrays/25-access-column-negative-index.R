744f67c8d5d78380fc4604f775bf53f115996c52
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-09-07 09:20:11 +0200

    Example 25: accessing column(s) by negative index

---
+++ 03-arrays/25-access-column-negative-index.R
@@ -0,0 +1,12 @@
+data <- 1:12
+dimension <- c(4, 3)
+
+row.names <- c("row1", "row2", "row3", "row4")
+column.names <- c("column1", "column2", "column3")
+
+a1 <- array(data, dim=dimension, dimnames=list(row.names, column.names))
+
+print(a1)
+print(a1[, -1])
+print(a1[, -2])
+print(a1[, -3])
