1fb02633d721ff54c1cb13d5d72eae5a5c6071aa
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-09-07 09:20:11 +0200

    Example 15: set rows and column names

---
+++ 03-arrays/15-rows-columns-names.R
@@ -0,0 +1,8 @@
+data <- 1:12
+dimension <- c(4, 3)
+
+row.names <- c("row1", "row2", "row3", "row4")
+column.names <- c("column1", "column2", "column3")
+
+a1 <- array(data, dim=dimension, dimnames=list(row.names, column.names))
+print(a1)
