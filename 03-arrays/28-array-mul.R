63bff696b1b0aa4d197a799cc5bbd9254a3f3d70
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-09-07 09:20:11 +0200

    Example 28: multiply items from two arrays

---
+++ 03-arrays/28-array-mul.R
@@ -0,0 +1,12 @@
+data1 <- c(1, 2, 3, 4, 5, 6)
+data2 <- -6:-1
+
+dimension <- c(3, 2)
+
+a1 <- array(data1, dim=dimension)
+print(a1)
+
+a2 <- array(data2, dim=dimension)
+print(a2)
+
+print(a1 * a2)
