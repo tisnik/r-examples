aa7f64d2dc34651f7e0ede9eb0596909cfcdf243
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-09-07 09:20:11 +0200

    Example 12: set vector dimensions to create 3D array

---
+++ 03-arrays/12-array-set-dimensions-3D.R
@@ -0,0 +1,17 @@
+print("2x3x4")
+a1 <- 1:24
+dimension1 <- c(2, 3, 4)
+dim(a1) <- dimension1
+print(a1)
+
+print("3x2x4")
+a2 <- 1:24
+dimension2 <- c(3, 2, 4)
+dim(a2) <- dimension2
+print(a2)
+
+print("4x3x2")
+a3 <- 1:24
+dimension3 <- c(4, 3, 2)
+dim(a3) <- dimension3
+print(a3)
