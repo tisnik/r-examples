294c77831feda6ef12c596ff2e67e7947a1e551f
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-09-08 09:05:13 +0200

    Example #3: matrix construction with different item ordering

---
+++ 04-matrix-table/03-matrix-construction-by-row.R
@@ -0,0 +1,19 @@
+data <- 1:12
+
+m1 <- matrix(data, nrow=1, ncol=12, byrow=TRUE)
+print(m1)
+
+m2 <- matrix(data, nrow=2, ncol=6, byrow=TRUE)
+print(m2)
+
+m3 <- matrix(data, nrow=3, ncol=4, byrow=TRUE)
+print(m3)
+
+m4 <- matrix(data, nrow=4, ncol=3, byrow=TRUE)
+print(m4)
+
+m5 <- matrix(data, nrow=6, ncol=2, byrow=TRUE)
+print(m5)
+
+m6 <- matrix(data, nrow=12, ncol=1, byrow=TRUE)
+print(m6)
