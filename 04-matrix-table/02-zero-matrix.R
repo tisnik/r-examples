601ec0c63e85b90f9525832f9eb643f2bce9ec04
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-09-08 09:05:13 +0200

    Example #2: zero matrix construction

---
+++ 04-matrix-table/02-zero-matrix.R
@@ -0,0 +1,19 @@
+data <- 0
+
+m1 <- matrix(data, nrow=1, ncol=12)
+print(m1)
+
+m2 <- matrix(data, nrow=2, ncol=6)
+print(m2)
+
+m3 <- matrix(data, nrow=3, ncol=4)
+print(m3)
+
+m4 <- matrix(data, nrow=4, ncol=3)
+print(m4)
+
+m5 <- matrix(data, nrow=6, ncol=2)
+print(m5)
+
+m6 <- matrix(data, nrow=12, ncol=1)
+print(m6)
