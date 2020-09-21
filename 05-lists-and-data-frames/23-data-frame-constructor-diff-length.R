e8723eac91d9f857d1858af7c4079ad7090e96ac
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-09-21 07:43:41 +0200

    Example 23: different length of data frame inputs

---
+++ 05-lists-and-data-frames/23-data-frame-constructor-diff-length.R
@@ -0,0 +1,8 @@
+languages <- data.frame(
+   id = c(1:20),
+   name = c("C", "Java", "Python", "C++", "C#", "Visual Basic", "JavaScript", "PHP", "R"),
+   usage = c(15.95, 13.48, 10.47, 7.11, 4.58, 4.12, 2.54),
+   change = c(0.74, -3.18, 0.59, 1.48, 1.18, 0.83)
+)
+
+print(languages)
