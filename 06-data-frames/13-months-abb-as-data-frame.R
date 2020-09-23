d662782efd9574794f56960aa99d80c262b21cd6
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-09-23 07:58:22 +0200

    Example 13: months in data frame

---
+++ 06-data-frames/13-months-abb-as-data-frame.R
@@ -0,0 +1,17 @@
+m <- matrix(
+  month.abb[c(12, 1:11)],
+  nrow = 3,
+  dimnames = list(
+    c("start", "middle", "end"),
+    c("Winter", "Spring", "Summer", "Fall")
+  )
+)
+
+print(class(m))
+print(m)
+
+print("-------------------------------")
+
+df <- as.data.frame(m)
+print(class(df))
+print(df)
