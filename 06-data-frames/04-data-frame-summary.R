27ea469d2f4057410dd134eb9e1ac5793ce0f920
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-09-23 07:58:22 +0200

    Example 04: data frame summary information

---
+++ 06-data-frames/04-data-frame-summary.R
@@ -0,0 +1,9 @@
+languages <- data.frame(
+   id = c (1:9), 
+   name = c("C", "Java", "Python", "C++", "C#", "Visual Basic", "JavaScript", "PHP", "R"),
+   usage = c(15.95, 13.48, 10.47, 7.11, 4.58, 4.12, 2.54, 2.49, 2.37), 
+   change = c(0.74, -3.18, 0.59, 1.48, 1.18, 0.83, 0.41, 0.62, 1.33),
+   stringsAsFactors = FALSE
+)
+
+print(summary(languages))
