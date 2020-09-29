56433beed16134d88b8b547c5193bb5d14883f0e
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-09-29 09:51:13 +0200

    Example 13: adding regression line to plot

---
+++ 07-plots/13_plot_regression.R
@@ -0,0 +1,16 @@
+input <- mtcars
+
+plot(x = input$wt,y = input$mpg,
+   xlab = "Weight",
+   ylab = "Mileage",
+   main = "Weight vs Mileage",
+   pch=20, col="red"
+)
+
+text(input$wt+0, input$mpg+0.7, rownames(input), cex=0.2)
+
+abline(20,0,col='blue', lty=2, lwd=1)
+abline(25,0,col='darkgreen', lty=2, lwd=1)
+
+regression <- lm(input$mpg~input$wt)
+abline(reg=regression, col='lightblue', lwd=2)
