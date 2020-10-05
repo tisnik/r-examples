02dce6f54717b151538c44976e2ae1bb7b60f152
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-10-05 08:31:40 +0200

    Example #18: free scales settings

---
+++ 08-lattice/18_xyplot_grid_free_scales.R
@@ -0,0 +1,9 @@
+library(lattice)
+
+input <- mtcars[,c('wt','mpg','gear')]
+
+xyplot(wt~mpg | gear,
+       data=input,
+       xlab = "Miles/(US) gallons",
+       ylab = "Weight (1000lbs)",
+       scales = "free")
