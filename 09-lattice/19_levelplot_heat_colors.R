e1334fc366007a100ab58018c1a2840621241cf0
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-10-15 09:30:30 +0200

    Example 19: levelplot using heat color palette.

---
+++ 09-lattice/19_levelplot_heat_colors.R
@@ -0,0 +1,17 @@
+library("lattice")
+
+x <- seq(1, 2*pi, length.out=200)
+y <- seq(1, 2*pi, length.out=200)
+
+r1 <- as.vector(sqrt(outer(x^2, y^2, "+")))
+r2 <- as.vector(sqrt(outer((x-3)^2, (y-3)^2, "+")))
+
+grid <- expand.grid(x=x, y=y)
+grid$z <- sin(r1) + sin(r2)
+
+levelplot(z ~ x*y,
+          data=grid,
+          xlab="",
+          ylab="",
+          main="",
+          col.regions = heat.colors(100))
