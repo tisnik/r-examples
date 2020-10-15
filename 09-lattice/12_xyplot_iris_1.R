6cfeecfdfd6805f84a690eacf0abb4a1e9cb053d
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-10-15 09:30:30 +0200

    Example 12: xyplot graph with modified layout and different data set.

---
+++ 09-lattice/12_xyplot_iris_1.R
@@ -0,0 +1,12 @@
+library(lattice)
+
+data("iris")
+head(iris)
+
+input <- iris[,c('Sepal.Length','Sepal.Width','Petal.Length','Petal.Width','Species')]
+
+xyplot(Sepal.Length ~ Petal.Length | Species,
+       group = Species,
+       data = input,
+       type = c("p", "smooth"),
+       scales = "free")
