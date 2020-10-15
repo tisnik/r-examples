2d2d72cc2f64537f96f004b715873767d6cf71e1
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-10-15 09:30:30 +0200

    Example 21: levelplot with contours displayed.

---
+++ 09-lattice/21_contours_terrain.R
@@ -0,0 +1,18 @@
+library("lattice")
+
+x <- seq(1.0, 2*pi, length.out=200)
+y <- seq(1.0, 2*pi, length.out=200)
+
+r1 <- as.vector(sqrt(outer(x^2, y^2, "+")))
+r2 <- as.vector(sqrt(outer((x-3)^2, (y-3)^2, "+")))
+
+grid <- expand.grid(x=x, y=y)
+grid$z <- sin(r1) + sin(r2)
+
+levelplot(z ~ x*y,
+          data=grid,
+          xlab="",
+          ylab="",
+          main="",
+          contour=TRUE,
+          col.regions = terrain.colors(20000))
