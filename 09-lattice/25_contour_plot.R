531fc593e119d3f9a4b559301d4d2683190ab3b5
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-10-15 09:30:30 +0200

    Example 25: contour plot with different color palette.

---
+++ 09-lattice/25_contour_plot.R
@@ -0,0 +1,14 @@
+library("lattice")
+
+x <- seq(1.0, 2*pi, length.out=200)
+y <- seq(1.0, 2*pi, length.out=200)
+
+r1 <- as.vector(sqrt(outer(x^2, y^2, "+")))
+r2 <- as.vector(sqrt(outer((x-3)^2, (y-3)^2, "+")))
+
+grid <- expand.grid(x=x, y=y)
+grid$z <- sin(r1) + sin(r2)
+
+contourplot(z ~ x * y, grid, col.regions = topo.colors(20),
+          at = c(-Inf, seq(-2.0, 2.0, by = 0.1), Inf))
+
