818ad29dd905deda77d6e84250708ae0fffc6629
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-10-15 09:30:30 +0200

    Example 07: basic bwplot.

---
+++ 09-lattice/07_bwplot.R
@@ -0,0 +1,18 @@
+library(lattice)
+
+input <- mtcars[,c('wt','mpg','hp','gear','cyl')]
+
+gear.f <- factor(input$gear,
+                 levels = c(3,4,5),
+                 labels = c("3gears","4gears","5gears"))
+
+cyl.f <- factor(input$cyl,
+                levels=c(4,6,8),
+                labels=c("4cyl","6cyl","8cyl")) 
+   
+bwplot(cyl.f~mpg|gear.f,
+   data = input,
+   ylab = "Cylinders",
+   xlab = "Miles per Gallon",
+   main = "Mileage by Cylinders and Gears",
+   layout = (c(1,3)))
