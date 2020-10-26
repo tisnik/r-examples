b6ddb5b0e65ae272eecec316b856c23809966373
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-10-26 08:03:45 +0100

    Example 17: manually specified color palette

---
+++ 11-ggplot2/17_scale_color_manual.R
@@ -0,0 +1,6 @@
+library(ggplot2)
+
+ggplot(data=mtcars) +
+       aes(x = mpg, y = wt, color=factor(cyl), size=factor(gear)) +
+       geom_point() +
+       scale_color_manual(values = c("4" = "red", "6" = "darkblue", "8" = "darkgreen"))
