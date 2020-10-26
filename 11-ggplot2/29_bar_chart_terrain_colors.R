f490e034380bdb77d8ddb073795b4f6bb00ade60
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-10-26 08:03:45 +0100

    Example 29: bar chart with terrain color palette

---
+++ 11-ggplot2/29_bar_chart_terrain_colors.R
@@ -0,0 +1,8 @@
+library(ggplot2)
+
+x <- seq(1, nrow(mtcars))
+
+ggplot(data=mtcars) +
+       aes(x = x, y = wt, fill=x) +
+       geom_bar(stat="identity") +
+       scale_fill_gradientn(colours=terrain.colors(4))
