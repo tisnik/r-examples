18187cefc5ce30d39d43270da3c17ffe0d089bf8
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-01-20 08:19:34 +0100

    If-else-return in function body

---
+++ 14-language-features/classify_number_1.R
@@ -0,0 +1,12 @@
+classify.number <- function(x) {
+  if (x > 0) {
+    result <- "Positive"
+  }
+  else if (x < 0) {
+    result <- "Negative"
+  }
+  else {
+    result <- "Zero"
+  }
+  return(result)
+}
